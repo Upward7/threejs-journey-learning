b374a1488f31bfa6dbaede75b149b3e10a86f548
Upward7 <3046831989@qq.com>
2025-02-07 22:26:27 +0800

    modify publicpath

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,7 @@ const {override} = require("customize-cra");
 
 module.exports = override(
     config => {
+        config.output.publicPath = "/threejs-outcomes/"
         config.output.path = require("path").resolve(__dirname, "docs");
         return config;
     }
